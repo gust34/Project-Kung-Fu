20f801f3740539e0c80625142c442e3aab8e2e50
Sidney Jr <44208657+sidjr12@users.noreply.github.com>
2019-12-06 20:44:22 -0300

    Home ainda em progresso...
    Fundo e cores meramente ilustrativos.

--- view/home.php
+++ view/home.php
@@ -4,9 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Importando Materialize CSS-->
+    <link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
+    <!--Importando CSS personalizado-->
+    <link type="text/css" rel="stylesheet" href="../materialize/css/custom.css"  media="screen,projection"/>
+    <!--Importando Ícones do Google-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <title>Home</title>
+
 </head>
-<body>
-    Home
+<body class="corpo">
+
+    <!--NavBar-->
+
+    <div class="navbar-fixed">
+
+        <nav class="red darken-2 z-depth-0">
+
+            <div class="nav-wrapper container">
+
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Aba 1</a></li>
+                <li><a href="#">Aba 2</a></li>
+                <li><a href="#">Aba 3</a></li>
+                <li><a href="#">Aba 4</a></li>
+                <li><a href="#"><i class="large material-icons">exit_to_app</i></a></li>
+            </ul>
+
+            </div>
+
+        </nav>
+    </div>
+<img src="../img/parallax5.jpg"class="confimg">
+    
+    <!--Importando Materialize JS-->
+    <script type="text/javascript" src="../materialize/js/jquery-1.12.1.min.js"></script>
+    <script type="text/javascript" src="../materialize/js/materialize.min.js"></script>
+
+    <script>
+
+    </script>
+
 </body>
 </html>
