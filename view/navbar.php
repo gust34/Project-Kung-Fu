3b39d0d25601f672bb0236361fb0aa1e59279de0
Sidney Jr <44208657+sidjr12@users.noreply.github.com>
2020-02-20 18:28:28 -0300

    NAVBAR
    SEM USO DEVIDO A BUGS E FALTA DE CONHECIMENTO

---
+++ view/navbar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Importando Materialize CSS-->
+    <link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
+    <!--Importando CSS personalizado-->
+    <link type="text/css" rel="stylesheet" href="../materialize/css/custom.css"  media="screen,projection"/>
+    <!--Importando Ícones do Google-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--NavBar/Cabeçalho-->
+    <title></title>  
+</head>
+
+<body>
+<!--NavBar/Cabeçalho-->
+<header>
+            <div class="navbar-fixed">
+
+                <nav class="red darken-3 z-depth-1">
+
+                    <div class="nav-wrapper container">
+                    <a href="home.php" class="brand-logo left"> <img class="responsive-img" src="">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down ">
+                        <li><a href="home.php">Home</a></li>
+                        <li><a href="ranking.php">Ranking</a></li>                        
+                        <li><a href="notice.php">Notícias</a></li>
+                        <li><a class="z-depth-1 waves-effect waves-light btn modal-trigger transparent" href="#modal1"><i class="large material-icons">lock</i></a>
+                    </ul>
+
+                    </div>
+
+                </nav>
+            </div>
+        </header>
+<!--NavBar/Cabeçalho-->
+</body>
+</html>
